400b0c57de9412ae258facee5c32734926bcc85b
Kapiven <pin231132@uvg.edu.gt>
2026-02-20 20:24:51 -0600

    feat: add index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import { io } from "socket.io-client"
+
+const socket = io("http://localhost:3000")
 
 function App() {
+  const [buffer, setBuffer] = useState([])
+
+  useEffect(() => {
+    socket.on("bufferUpdate", (data) => {
+      setBuffer(data)
+    })
+
+    return () => {
+      socket.off("bufferUpdate")
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", fontFamily: "Arial" }}>
+      <h1>Estado del Buffer</h1>
+      <div>
+        {buffer.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              display: "inline-block",
+              width: "60px",
+              height: "60px",
+              margin: "5px",
+              lineHeight: "60px",
+              border: "2px solid black",
+              fontWeight: "bold"
+            }}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
